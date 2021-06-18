b0a3538156445e0d650a08f526114b86fb7b5987
Leolionross <Leolionross@gmail.com>
2021-06-18 18:19:39 -0400

    ok

--- windows 9/Form1.vb
+++ windows 9/Form1.vb
@@ -11,9 +11,6 @@
     End Sub
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Cursor = Cursors.AppStarting
-        MsgBox("This program is not malware, but it is a joke", 0 + 64, "ATTENTION")
-        MsgBox("So if someone sent this to you telling you this is an actual windows 9 upgrade, they are lying", 0 + 64, "ATTENTION")
-        MsgBox("But if you still want to go for the experience, then go right ahead! :)", 0 + 64, "ATTENTION")
         Timer1.Enabled = True
     End Sub
 
@@ -21,7 +18,7 @@
         ProgressBar1.Value = 100
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) 
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
         Close()
     End Sub
 End Class
